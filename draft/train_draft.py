bd11c54f7c788e90b8fd5d815eba177434c1eff7
dreaming-panda <chenzhuoming911@gmail.com>
2025-02-13 23:23:46 -0500

    draft

---
+++ draft/train_draft.py
@@ -0,0 +1,76 @@
+import torch
+from transformers import AutoTokenizer, DataCollatorForLanguageModeling, TrainingArguments, Trainer, AutoModelForCausalLM, Qwen2ForCausalLM
+from datasets import load_dataset
+import json
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--tokenizer', type=str, default="mistralai/Mistral-Small-24B-Instruct-2501",help='tokenizer')
+parser.add_argument('--config', type=str, default="./config.json",help='model config')
+parser.add_argument('--bsz', type=int, default=4, help='generation length')
+args = parser.parse_args()
+
+with open(args.config, "r") as f:
+    config = json.load(f)
+
+model_name = args.model
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model = AutoModelForCausalLM.from_config(config)
+
+train_data_files= [
+        "train/chunk1/example_train_10*.jsonl.zst", 
+        "train/chunk1/example_train_11*.jsonl.zst", 
+        "train/chunk1/example_train_12*.jsonl.zst",
+        "train/chunk1/example_train_13*.jsonl.zst"
+]
+train_raw_datasets = load_dataset("cerebras/SlimPajama-627B", data_files=train_data_files, split="train")
+
+eval_data_files= ["validation/chunk1/example_holdout_*.jsonl.zst"]
+eval_raw_datasets = load_dataset("cerebras/SlimPajama-627B", data_files=eval_data_files, split="train")
+
+# 定义预处理函数：对句子对进行编码
+def preprocess_function(examples):
+    
+    
+    output = tokenizer(
+        examples["problem"] + "\n\n" + examples["solution"],
+        truncation=True,
+        max_length=2048,
+        padding="max_length"
+    )
+
+    return output
+
+train_tokenized_datasets = train_raw_datasets.map(preprocess_function, batched=False)
+eval_tokenized_datasets = eval_raw_datasets.map(preprocess_function, batched=False)
+
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    learning_rate=1e-4,
+    per_device_train_batch_size=args.bsz,
+    per_device_eval_batch_size=args.bsz,
+    weight_decay=0.01,
+    lr_scheduler_type="cosine",
+    load_best_model_at_end=True,
+    logging_dir='./logs',
+    logging_steps=10,
+    bf16=True,
+    save_only_model=True,
+    save_steps=5000,
+    save_total_limit=2
+)
+
+# 初始化 Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_tokenized_datasets,
+    eval_dataset=eval_tokenized_datasets,
+    tokenizer=tokenizer,
+    data_collator=data_collator
+)
+
+# 开始训练
+trainer.train()
